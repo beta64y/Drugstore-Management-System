8c7772a634e4caa27fcc293f10254edfb190dc53
beta64y <106177539+beta64y@users.noreply.github.com>
2022-07-29 19:21:05 +0300

    Add project files.

---
+++ Drugstore Management System/Database/Models/DrugStore/DrugStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drugstore_Management_System.Database.Models.DrugStore
+{
+    internal class DrugStore
+    {
+    }
+}
---
+++ Drugstore Management System/Database/Models/Users/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drugstore_Management_System.Database.Models.Users
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        protected int IdCounter { get; set; } = 1;
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Fin { get; set; }
+        public User(int id, string name, string surName, string fin)
+        {
+            Id = id;
+            IdCounter = IdCounter++;
+            Name = name;
+            SurName = surName;
+            Fin = fin;
+        }
+    }
+}
